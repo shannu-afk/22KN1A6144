8d59461de679af1f31a3869031c5fc5505101c81
k.shanmukh <kodalishannu666@gmail.com>
2025-08-02 12:28:11 +0530

    Update middleware.py

--- middleware.py
+++ middleware.py
@@ -1,16 +1,11 @@
 from datetime import datetime
-
-
 class LoggingMiddleware:
     def __init__(self, app):
         self.app = app
-
     def __call__(self, environ, start_response):
         method = environ.get("REQUEST_METHOD")
         path = environ.get("PATH_INFO")
         timestamp = datetime.utcnow().isoformat()
-
         with open("logs.txt", "a") as log_file:
             log_file.write(f"[{timestamp}] {method} {path}\n")
-
         return self.app(environ, start_response)
